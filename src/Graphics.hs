7ce3a8ddea96aafc2a66a31cae00c0139bc537cc
Christopher Lege <christopher.lege@googleemail.com>
2012-02-27 16:58:53 +0100

    rotated

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -18,6 +18,7 @@ display = do
   loadIdentity
   color $ Color3 (2.3::GLfloat) (1.2::GLfloat) (1.1::GLfloat)
   scale (0.7::GLfloat) (0.7::GLfloat) (0.7::GLfloat)
+  rotate (90.0::GLfloat) (Vector3 1.0 1.0 1.0)
   oneCube (0.2::GLfloat)
   swapBuffers
 
