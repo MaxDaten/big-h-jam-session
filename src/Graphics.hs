e19fbfdef535ebe644de399afd0702cffa4c4535
Christopher Lege <christopher.lege@googleemail.com>
2012-02-27 15:20:34 +0100

    Graphics.hs

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -6,17 +6,16 @@ import Graphics.UI.GLUT
 import Data.IORef
 
 oneCube a = do 
-	renderPrimitve Quads do
+	renderPrimitive Quads $ do
 		vertex $ Vertex3 a a a
-		vertex $ Vertex3 -a a a
-		vertex $ Vertex3 a -a a
-		vertex $ Vertex3 -a -a a
+		vertex $ Vertex3 (-a) a a
+		vertex $ Vertex3 a (-a) a
+		vertex $ Vertex3 (-a) (-a) a
 
 
-display angle = do 
+display = do 
   clear [ColorBuffer]
   loadIdentity
- 
   rotate a $ Vector3 0 0 (0.5::GLfloat)
   scale 0.7 0.7 (0.7::GLfloat)
   color $ Color3 2.3 1.2 1.1
