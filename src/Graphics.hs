3e1ad5f828aa39cfb936c5262906dddabf9c062a
Christopher Lege <christopher.lege@googleemail.com>
2012-02-27 15:12:16 +0100

    Graphics Module

---
+++ src/Graphics.hs
@@ -0,0 +1,25 @@
+module Graphics (display)
+where
+
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLUT
+import Data.IORef
+
+oneCube a = do 
+	renderPrimitve Quads do
+		vertex $ Vertex3 a a a
+		vertex $ Vertex3 -a a a
+		vertex $ Vertex3 a -a a
+		vertex $ Vertex3 -a -a a
+
+
+display angle = do 
+  clear [ColorBuffer]
+  loadIdentity
+ 
+  rotate a $ Vector3 0 0 (0.5::GLfloat)
+  scale 0.7 0.7 (0.7::GLfloat)
+  color $ Color3 2.3 1.2 1.1
+  oneCube (0.2::GLfloat)
+  swapBuffers
+
--- src/bigH.hs
+++ src/bigH.hs
@@ -1,16 +1,18 @@
-
 import System.Environment
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
+import Graphics
  
 -- | 'main' runs the main program
 main :: IO ()
 main =    do
             (progname, _) <- getArgsAndInitialize
-            createWindow "Hello World"
+            initialDisplayMode $= [DoubleBuffered]
+            createWindow "BigHS"
             displayCallback $= display
             mainLoop
- 
+{- 
 display :: IO ()
 display = do
   clear [ ColorBuffer ]; flush
+  -}
