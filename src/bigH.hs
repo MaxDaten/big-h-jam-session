431024339b736accefd3bebe39d8787df7adeb76
Jan-Philip Loos <maxdaten@gmail.com>
2012-03-02 00:16:34 +0100

    Added a 1sec spam function

--- src/bigH.hs
+++ src/bigH.hs
@@ -12,6 +12,7 @@ main =    do
             initialDisplayMode $= [DoubleBuffered]
             createWindow "BigHS"
             windowSize $= Size 800 800
+            addTimerCallback 1000 (timer)
             displayCallback $= display
             mainLoop
  
@@ -20,3 +21,9 @@ display = do
   clear [ ColorBuffer ]
   drawScene
   swapBuffers; flush
+ 
+-- A test timer function to spam the stdout every 1 sec
+timer :: TimerCallback
+timer = do
+        putStrLn $ "tick"
+        addTimerCallback 1000 (timer) -- recall in 1000 msecs
